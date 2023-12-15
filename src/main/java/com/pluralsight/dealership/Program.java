36f3c97c48b92de9100ce71eafa820a2e395fbfd
Randy23123 <rlopez@appdev.yearup.org>
2023-12-15 11:48:41 -0500

    xml depends and datasource

--- src/main/java/com/pluralsight/dealership/Program.java
+++ src/main/java/com/pluralsight/dealership/Program.java
@@ -1,10 +1,25 @@
 package com.pluralsight.dealership;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.io.IOException;
-import java.io.Reader;
+
 
 public class Program {
     public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            return;
+        }
+        String username = args[0];
+        String password = args[1];
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/car_dealership");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+
+
         Dealership.dealership();
     }
 }
